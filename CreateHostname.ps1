813543d72b661ca41ff547553935e7b7891309df
sgulloni <steve.gulloni@hcl.com>
2016-03-28 14:04:34 -0300

    Added files via upload

---
+++ CreateHostname.ps1
@@ -0,0 +1,44 @@
+import-module activedirectory
+
+## Device Naming Convention
+$naming = "AACLLTOL"
+
+## First Hostname Number to create
+[int]$i = 51 
+
+## OU where to create
+$ou = "OU=CLLTO,OU=Laptops,OU=Clients,OU=BASE,OU=AA,DC=anglo,DC=local"
+
+## Range to Create, in this example it will be from 51 till 101
+while ($i -lt 102) {
+    
+    ## Check if Hostname number is under 100
+    if ($i -lt 100) {
+    
+    #If under 100, add "00"
+    $pcName = $naming + "00" + $i
+    } 
+    
+    #If above 100, add "0"
+    else {
+    $pcName = $naming + "0" + $i
+    }
+    
+    ### Create Hostname ##
+    New-AdComputer -name $pcName -SamAccountName $pcName -Path $ou -enable $True -description "Created - SRTASK0353832"
+    
+    ## To Hostnames, samAccountName should include "$" at the end.
+    $pcName += "$"
+    
+    #Add Groups to Hostname
+    ADD-ADGroupMember “AA MBAM Win 8.1 Computers” –members $pcName
+    ADD-ADGroupMember “HPsam-BASE-CLLTO-Desktops” –members $pcName
+    
+    #Status Indicator
+    write-host "Hostname " $pcName " Created"
+    
+    ## Sum 1 to Hostname number.
+    $i++
+}
+
+write-host "Task Completed"
